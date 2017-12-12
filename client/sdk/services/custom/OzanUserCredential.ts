cfadd057039574ff159c83fbccf795021422657a
wira <swira110@gmail.com>
2017-12-13 05:15:22 +0700

    update

--- client/sdk/services/custom/OzanUserCredential.ts
+++ client/sdk/services/custom/OzanUserCredential.ts
@@ -519,7 +519,6 @@ export class OzanUserCredentialApi extends BaseLoopBackApi {
   }
 
   /**
-<<<<<<< HEAD
    * <em>
          * (The remote method definition does not provide any description.)
          * </em>
@@ -551,8 +550,37 @@ export class OzanUserCredentialApi extends BaseLoopBackApi {
   }
 
   /**
-=======
->>>>>>> develop
+   * <em>
+         * (The remote method definition does not provide any description.)
+         * </em>
+   *
+   * @param {object} data Request data.
+   *
+   * This method expects a subset of model properties as request parameters.
+   *
+   * @returns {object[]} An empty reference that will be
+   *   populated with the actual data once the response is returned
+   *   from the server.
+   *
+   * <em>
+   * (The remote method definition does not provide any description.
+   * This usually means the response is a `OzanUserCredential` object.)
+   * </em>
+   */
+  public ozanLogout(params: any, customHeaders?: Function): Observable<any> {
+    let _method: string = "POST";
+    let _url: string = LoopBackConfig.getPath() + "/" + LoopBackConfig.getApiVersion() +
+    "/OzanUserCredentials/ozanLogout";
+    let _routeParams: any = {};
+    let _postBody: any = {
+      params: params
+    };
+    let _urlParams: any = {};
+    let result = this.request(_method, _url, _routeParams, _urlParams, _postBody, null, customHeaders);
+    return result;
+  }
+
+  /**
    * Creates a new instance in accessTokens of this model.
    *
    * @param {any} id OzanUserCredential id
