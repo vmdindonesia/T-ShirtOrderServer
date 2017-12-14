2079a157739fc0bec2addec8ea299882aec0261e
atsun <adjietirta13@gmail.com>
2017-12-14 20:47:30 +0700

    server aji task

--- server/create-table.js
+++ server/create-table.js
@@ -1,6 +1,6 @@
 var server = require('./server');
-var ds = server.dataSources.ozan;
-var lbTables = ['OzanUserCredential', 'OzanOrder', 'OzanLibrary'];
+var ds = server.dataSources.ozanDB;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'OzanUserCredential', 'OzanOrder', 'OzanLibrary'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
