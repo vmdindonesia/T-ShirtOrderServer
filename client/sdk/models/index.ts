9d362ec281e3f67af6be4066af32ff510238676e
atsun <adjietirta13@gmail.com>
2017-12-14 23:15:56 +0700

    update

--- client/sdk/models/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-export * from './User';
-export * from './OzanUserCredential';
-export * from './OzanOrder';
-export * from './OzanLibrary';
-export * from './BaseModels';
-export * from './FireLoopRef';
--- client/sdk/services/custom/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-export * from './User';
-export * from './OzanUserCredential';
-export * from './OzanOrder';
-export * from './OzanLibrary';
-export * from './SDKModels';
-export * from './logger.service';
