d78bffe967c60641cf3a8c114d6c470b62f9ab28
Ren <rendy1429@gmail.com>
2017-12-18 04:17:02 +0700

    change setting default

--- server/create-table.js
+++ server/create-table.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.ozanDB;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'OzanUserCredential', 'OzanOrder', 'OzanLibrary'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'OzanUserCredential', 'OzanOrder', 'OzanLibrary', 'OzanOrderProduct'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
