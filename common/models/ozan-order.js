05e187172f9afff0ae08d71632ec43834b1da1eb
atsun <adjietirta13@gmail.com>
2017-12-20 03:14:07 +0700

    add fungsion view report order (down payment and payed)

--- common/models/ozan-order.js
+++ common/models/ozan-order.js
@@ -79,17 +79,88 @@ module.exports = function (Ozanorder) {
                 cb(null, accounts);
             }
          });
-        // Ozanorder.find({
-        //     where: {
-        //         userId: params.userId
-        //     }
-        // }, function (err, buyGet) {
-        //     if (err) {
-        //         cb(err);
-        //         console.log(err, 'Errornya');
-        //     } else {
-        //         cb(null, buyGet);
-        //     }
-        // });
+
     }
+
+    /**
+     * function get data report Down Payment
+     */
+    Ozanorder.remoteMethod(
+        'ozanDownPaymentReport', {
+            accepts: [{
+                arg: 'params',
+                type: 'Object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'ozanDownPaymentReport', type: 'array', root: true
+            },
+            http: {
+                path: '/ozanDownPaymentReport',
+                verb: 'get'
+            }
+        });
+
+
+    Ozanorder.ozanDownPaymentReport = function (params, options, cb) {
+        console.log(params.userId, 'Params');
+        Ozanorder.find({
+            where: {
+                status: 2
+            }}, function(err, accounts) { 
+            if (err) {
+                cb(err);
+                console.log(err, 'Errornya');
+            } else {
+                cb(null, accounts);
+            }
+         });
+    }
+
+        /**
+     * function get data report Payed
+     */
+    Ozanorder.remoteMethod(
+        'ozanPayedReport', {
+            accepts: [{
+                arg: 'params',
+                type: 'Object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'ozanPayedReport', type: 'array', root: true
+            },
+            http: {
+                path: '/ozanPayedReport',
+                verb: 'get'
+            }
+        });
+
+
+    Ozanorder.ozanPayedReport = function (params, options, cb) {
+        console.log(params.userId, 'Params');
+        Ozanorder.find({
+            where: {
+                status: 3
+            }}, function(err, accounts) { 
+            if (err) {
+                cb(err);
+                console.log(err, 'Errornya');
+            } else {
+                cb(null, accounts);
+            }
+         });
+    }
+
+
 };
